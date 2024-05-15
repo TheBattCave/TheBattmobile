ee5e0c2a98c76a28717cec70bc1be808744b686c
Dave Beck <dacb@uw.edu>
2024-05-15 12:41:53 -0700

    added example (empty) .zip file and test code to reference it

--- alfred/tests/test_etl.py
+++ alfred/tests/test_etl.py
@@ -1,3 +1,17 @@
+import os
+import unittest
+
 import alfred
 
+data_path = os.path.join(alfred.__path__[0], 'data')
+
 # tests on alfred.etl
+class test_ETL(unittest.TestCase):
+
+    def test_sort(self):
+        """
+        Testing the sort functions or some other description here.
+        """
+
+        # example of reading a df from a zip using data_path above
+        df = pd.read_csv(os.path.join(data_path, "test_data.zip"))
